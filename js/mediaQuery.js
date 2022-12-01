3a4cd9b3ecdb8d0e484f46555bd2d770b44113eb
LobitoM365 <almariojuancho@gmail.com>
2022-12-01 09:13:43 -0500

    probando modificaciones

--- js/mediaQuery.js
+++ js/mediaQuery.js
@@ -5,11 +5,11 @@ let mainMenu = document.getElementById("main_menu")
 hamburguer.addEventListener("click", function () {
 
     if (mainMenu.classList.contains("main_menu_visible")) { 
-        mainMenu.classList.remove("main_menu_visible");
-        mainMenu.classList.add("main_Menu");
-    }
-    else {
         mainMenu.classList.add("main_menu_visible");
         mainMenu.classList.remove("main_Menu");
+    }
+    else {
+        mainMenu.classList.remove("main_menu_visible");
+        mainMenu.classList.add("main_Menu");
     }
 });
