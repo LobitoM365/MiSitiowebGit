d0c08f3bc28d51166454f79c3412bd34e6f73b94
LobitoM365 <almariojuancho@gmail.com>
2022-12-05 21:33:18 -0500

    probando modificaciones

--- js/script.js
+++ js/script.js
@@ -1,15 +1,153 @@
 let iconHamburguer = document.getElementById("icon_hamburguer");
 let menuPrincipal = document.getElementById("menuPrincipal");
+let iconSubMenuPrincipal = document.getElementById("iconSubMenuPrincipal");
+let subMenuPrincipal = document.getElementById("subMenuPrincipal");
+let iconSubMenu = document.getElementById("iconSubMenu");
+let subMenu = document.getElementById("subMenu");
+let iconSubMenu2 = document.getElementById("iconSubMenu2");
+let subMenu2 = document.getElementById("subMenu2");
+let iconSubMenu3 = document.getElementById("iconSubMenu3");
+let subMenu3 = document.getElementById("subMenu3");
+let iconSubMenu4 = document.getElementById("iconSubMenu4");
+let subMenu4 = document.getElementById("subMenu4");
+let iconSubMenu5 = document.getElementById("iconSubMenu5");
+let subMenu5 = document.getElementById("subMenu5");
+let iconSubMenu6 = document.getElementById("iconSubMenu6");
+let subMenu6 = document.getElementById("subMenu6");
+let iconSubMenu7 = document.getElementById("iconSubMenu7");
+let subMenu7 = document.getElementById("subMenu7");
+let iconSubMenu8 = document.getElementById("iconSubMenu8");
+let subMenu8 = document.getElementById("subMenu8");
+let iconSubMenu9 = document.getElementById("iconSubMenu9");
+let subMenu9 = document.getElementById("subMenu9");
+let iconSubMenu10 = document.getElementById("iconSubMenu10");
+let subMenu10 = document.getElementById("subMenu10");
+let iconSubMenu11 = document.getElementById("iconSubMenu11");
+let subMenu11 = document.getElementById("subMenu11");
+let iconSubMenu12 = document.getElementById("iconSubMenu12");
+let subMenu12 = document.getElementById("subMenu12");
+let iconSubMenu13 = document.getElementById("iconSubMenu13");
+let subMenu13 = document.getElementById("subMenu13");
 
 iconHamburguer.addEventListener("click" ,function(){
-
     if(menuPrincipal.classList.contains("menuPrincipalInvisible")){
         menuPrincipal.classList.remove("menuPrincipalInvisible")
     } else{
         menuPrincipal.classList.add("menuPrincipalInvisible")
     }
 }
-
+)
+iconSubMenuPrincipal.addEventListener("click" ,function(){
+    if(subMenuPrincipal.classList.contains("subMenuPrincipalInvisible")){
+        subMenuPrincipal.classList.remove("subMenuPrincipalInvisible")
+    } else{
+        subMenuPrincipal.classList.add("subMenuPrincipalInvisible")
+    }
+}
+)
+iconSubMenu.addEventListener("click" ,function(){
+    if(subMenu.classList.contains("subMenuInvisible")){
+        subMenu.classList.remove("subMenuInvisible")
+    } else{
+        subMenu.classList.add("subMenuInvisible")
+    }
+}
+)
+iconSubMenu2.addEventListener("click" ,function(){
+    if(subMenu2.classList.contains("subMenuInvisible2")){
+        subMenu2.classList.remove("subMenuInvisible2")
+    } else{
+        subMenu2.classList.add("subMenuInvisible2")
+    }
+}
+)
+iconSubMenu3.addEventListener("click" ,function(){
+    if(subMenu3.classList.contains("subMenuInvisible3")){
+        subMenu3.classList.remove("subMenuInvisible3")
+    } else{
+        subMenu3.classList.add("subMenuInvisible3")
+    }
+}
+)
+iconSubMenu4.addEventListener("click" ,function(){
+    if(subMenu4.classList.contains("subMenuInvisible4")){
+        subMenu4.classList.remove("subMenuInvisible4")
+    } else{
+        subMenu4.classList.add("subMenuInvisible4")
+    }
+}
+)
+iconSubMenu5.addEventListener("click" ,function(){
+    if(subMenu5.classList.contains("subMenuInvisible5")){
+        subMenu5.classList.remove("subMenuInvisible5")
+    } else{
+        subMenu5.classList.add("subMenuInvisible5")
+    }
+}
+)
+iconSubMenu6.addEventListener("click" ,function(){
+    if(subMenu6.classList.contains("subMenuInvisible6")){
+        subMenu6.classList.remove("subMenuInvisible6")
+    } else{
+        subMenu6.classList.add("subMenuInvisible6")
+    }
+}
+)
+iconSubMenu7.addEventListener("click" ,function(){
+    if(subMenu7.classList.contains("subMenuInvisible7")){
+        subMenu7.classList.remove("subMenuInvisible7")
+    } else{
+        subMenu7.classList.add("subMenuInvisible7")
+    }
+}
+)
+iconSubMenu8.addEventListener("click" ,function(){
+    if(subMenu8.classList.contains("subMenuInvisible8")){
+        subMenu8.classList.remove("subMenuInvisible8")
+    } else{
+        subMenu8.classList.add("subMenuInvisible8")
+    }
+}
+)
+iconSubMenu9.addEventListener("click" ,function(){
+    if(subMenu9.classList.contains("subMenuInvisible9")){
+        subMenu9.classList.remove("subMenuInvisible9")
+    } else{
+        subMenu9.classList.add("subMenuInvisible9")
+    }
+}
+)
+iconSubMenu10.addEventListener("click" ,function(){
+    if(subMenu10.classList.contains("subMenuInvisible10")){
+        subMenu10.classList.remove("subMenuInvisible10")
+    } else{
+        subMenu10.classList.add("subMenuInvisible10")
+    }
+}
+)
+iconSubMenu11.addEventListener("click" ,function(){
+    if(subMenu11.classList.contains("subMenuInvisible11")){
+        subMenu11.classList.remove("subMenuInvisible11")
+    } else{
+        subMenu11.classList.add("subMenuInvisible11")
+    }
+}
+)
+iconSubMenu12.addEventListener("click" ,function(){
+    if(subMenu12.classList.contains("subMenuInvisible12")){
+        subMenu12.classList.remove("subMenuInvisible12")
+    } else{
+        subMenu12.classList.add("subMenuInvisible12")
+    }
+}
+)
+iconSubMenu13.addEventListener("click" ,function(){
+    if(subMenu13.classList.contains("subMenuInvisible13")){
+        subMenu13.classList.remove("subMenuInvisible13")
+    } else{
+        subMenu13.classList.add("subMenuInvisible13")
+    }
+}
 )
 
 
