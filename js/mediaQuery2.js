bd8af926f9bc83b426adf941ce14ad6c827496b3
LobitoM365 <almariojuancho@gmail.com>
2022-12-29 15:52:04 -0500

    probando modificaciones

--- js/mediaQuery2.js
+++ js/mediaQuery2.js
@@ -1,23 +1,36 @@
 let icono = document.getElementById("icon_hamburguer")
 let desplegarSubmenu = document.getElementById("desplegar_submenu")
-let menu  =  document.getElementById("menu")
-let submenu  =  document.getElementById("submenu")
+let menu = document.getElementById("menu")
+let submenu = document.getElementById("submenu")
+let nav = document.getElementById("nav");
 
-icono.addEventListener("click", function(){
-    if( menu.classList.contains("menu_invisible")){
+icono.addEventListener("click", function () {
+    if (menu.classList.contains("menu_invisible")) {
         menu.classList.remove("menu_invisible")
-    }else{
+    } else {
         menu.classList.add("menu_invisible")
+        submenu.classList.add("submenu_invisible")
+    }
+    if (nav.classList.contains("navTransition")) {
+        nav.classList.remove("navTransition")
+        nav.classList.add("navTransition2")
+    } else {
+        nav.classList.add("navTransition")
+        nav.classList.remove("navTransition2")
     }
 })
 
 
-desplegarSubmenu.addEventListener("click" , function(){
-    
-if( submenu.classList.contains("submenu_invisible")){
-    submenu.classList.remove("submenu_invisible")
-}else{
-    submenu.classList.add("submenu_invisible")
-}
+desplegarSubmenu.addEventListener("click", function () {
+
+    if (submenu.classList.contains("submenu_invisible")) {
+        submenu.classList.remove("submenu_invisible")
+        submenu.classList.add("submenu_invisible2")
+        
+    } else {
+        submenu.classList.add("submenu_invisible")
+        submenu.classList.remove("submenu_invisible2")
+    }
 
 })
+
