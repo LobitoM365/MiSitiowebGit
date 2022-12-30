4d9d95ce834e4e491f632949cd33957d38b8b9ab
LobitoM365 <almariojuancho@gmail.com>
2022-12-30 18:46:14 -0500

    probando modificaciones

--- js/mediaQuery2.js
+++ js/mediaQuery2.js
@@ -3,14 +3,11 @@ let desplegarSubmenu = document.getElementById("desplegar_submenu")
 let menu = document.getElementById("menu")
 let submenu = document.getElementById("submenu")
 let nav = document.getElementById("nav");
+let transitionMargin = document.getElementById("transitionMargin");
+let idParrafo = document.getElementById("idParrafo");
 
 icono.addEventListener("click", function () {
-    if (menu.classList.contains("menu_invisible")) {
-        menu.classList.remove("menu_invisible")
-    } else {
-        menu.classList.add("menu_invisible")
-        submenu.classList.add("submenu_invisible")
-    }
+
     if (nav.classList.contains("navTransition")) {
         nav.classList.remove("navTransition")
         nav.classList.add("navTransition2")
@@ -18,19 +15,38 @@ icono.addEventListener("click", function () {
         nav.classList.add("navTransition")
         nav.classList.remove("navTransition2")
     }
+    if (submenu.classList.contains("submenu_invisible2")) {
+        submenu.classList.add("submenu_invisible")
+        submenu.classList.remove("submenu_invisible2")
+        transitionMargin.classList.remove("margin2");
+        transitionMargin.classList.add("margin");
+    }
 })
-
 
 desplegarSubmenu.addEventListener("click", function () {
 
     if (submenu.classList.contains("submenu_invisible")) {
         submenu.classList.remove("submenu_invisible")
         submenu.classList.add("submenu_invisible2")
-        
+        transitionMargin.classList.remove("margin");
+        transitionMargin.classList.add("margin2");
+
     } else {
         submenu.classList.add("submenu_invisible")
         submenu.classList.remove("submenu_invisible2")
+        transitionMargin.classList.remove("margin2");
+        transitionMargin.classList.add("margin");
     }
 
+})
+idParrafo.addEventListener("click", function () {
+    if (nav.classList.contains("navTransition")) {
+        nav.classList.remove("navTransition")
+        nav.classList.add("navTransition2")
+        submenu.classList.add("submenu_invisible")
+        submenu.classList.remove("submenu_invisible2")
+        transitionMargin.classList.remove("margin2");
+        transitionMargin.classList.add("margin");
+    }
 })
 
