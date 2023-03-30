479e03b5a4545ed34387e0dbf6f4d84bb8697b06
LobitoM365 <almariojuancho1@gmail.com>
2023-03-30 14:42:07 -0500

    probando modificaciones

--- js/carritoCompras.js
+++ js/carritoCompras.js
@@ -1,32 +1,34 @@
-let imgBox1 = document.getElementById("imgBox1");
-let imgBox2 = document.getElementById("imgBox2");
-let imgBox3 = document.getElementById("imgBox3");
-let imgBox4 = document.getElementById("imgBox4");
+let idImgMain1 = document.getElementById("idImgMain1");
+let idImgMain2 = document.getElementById("idImgMain2");
+let idImgMain3 = document.getElementById("idImgMain3");
+let idImgMain4 = document.getElementById("idImgMain4");
 let imgMini1 = document.getElementById("imgMini1");
 let imgMini2 = document.getElementById("imgMini2");
 let imgMini3 = document.getElementById("imgMini3");
 let imgMini4 = document.getElementById("imgMini4");
-let idFocus1 = document.getElementById("idFocus1")
-let idFocus2 = document.getElementById("idFocus2")
-let idFocus3 = document.getElementById("idFocus3")
-let idFocus4 = document.getElementById("idFocus4")
-let idImgMain = document.getElementById("idImgMain")
+let idFocus1 = document.getElementById("idFocus1");
+let idFocus2 = document.getElementById("idFocus2");
+let idFocus3 = document.getElementById("idFocus3");
+let idFocus4 = document.getElementById("idFocus4");
+let idImgMain = document.getElementById("idImgMain");
+let imgPredeterminada = document.getElementById("imgPredeterminada");
 
 imgMini1.addEventListener("click", function () {
+    imgPredeterminada.classList.add("img-box-invisible")
     /*     idImgMain.setAttribute("src", "img/imgCarrito/image-product-1.jpg") */
     if (imgMini1.classList.contains("img-opacity")) {
         imgMini1.classList.remove("img-opacity");
         idFocus1.classList.remove("focus")
-        imgBox1.classList.remove("img-main-animation1")
+        idImgMain1.classList.remove("img-main-animation1")
     } else {
-        imgBox1.classList.add("img-main-animation1")
-        imgBox2.classList.remove("img-main-animation2")
-        imgBox3.classList.remove("img-main-animation3")
-        imgBox4.classList.remove("img-main-animation4")
-        imgBox1.classList.remove("img-box-invisible")
-        imgBox2.classList.add("img-box-invisible")
-        imgBox3.classList.add("img-box-invisible")
-        imgBox4.classList.add("img-box-invisible")
+        idImgMain1.classList.add("img-main-animation1")
+        idImgMain2.classList.remove("img-main-animation2")
+        idImgMain3.classList.remove("img-main-animation3")
+        idImgMain4.classList.remove("img-main-animation4")
+        idImgMain1.classList.remove("img-box-invisible")
+        idImgMain2.classList.add("img-box-invisible")
+        idImgMain3.classList.add("img-box-invisible")
+        idImgMain4.classList.add("img-box-invisible")
         imgMini1.classList.add("img-opacity");
         idFocus1.classList.add("focus")
         idFocus2.classList.remove("focus")
@@ -38,20 +40,21 @@ imgMini1.addEventListener("click", function () {
     }
 })
 imgMini2.addEventListener("click", function () {
+    imgPredeterminada.classList.add("img-box-invisible")
     /* idImgMain.setAttribute("src", "img/imgCarrito/image-product-2.jpg") */
     if (imgMini2.classList.contains("img-opacity")) {
         imgMini2.classList.remove("img-opacity");
         idFocus2.classList.remove("focus")
-        imgBox2.classList.remove("img-main-animation2")
+        idImgMain2.classList.remove("img-main-animation2")
     } else {
-        imgBox2.classList.add("img-main-animation2")
-        imgBox3.classList.remove("img-main-animation3")
-        imgBox4.classList.remove("img-main-animation4")
-        imgBox1.classList.remove("img-main-animation1")
-        imgBox2.classList.remove("img-box-invisible")
-        imgBox1.classList.add("img-box-invisible")
-        imgBox3.classList.add("img-box-invisible")
-        imgBox4.classList.add("img-box-invisible")
+        idImgMain2.classList.add("img-main-animation2")
+        idImgMain3.classList.remove("img-main-animation3")
+        idImgMain4.classList.remove("img-main-animation4")
+        idImgMain1.classList.remove("img-main-animation1")
+        idImgMain2.classList.remove("img-box-invisible")
+        idImgMain1.classList.add("img-box-invisible")
+        idImgMain3.classList.add("img-box-invisible")
+        idImgMain4.classList.add("img-box-invisible")
         imgMini2.classList.add("img-opacity");
         idFocus2.classList.add("focus")
         idFocus1.classList.remove("focus")
@@ -64,20 +67,21 @@ imgMini2.addEventListener("click", function () {
     }
 })
 imgMini3.addEventListener("click", function () {
+    imgPredeterminada.classList.add("img-box-invisible")
     /* idImgMain.setAttribute("src", "img/imgCarrito/image-product-3.jpg") */
     if (imgMini3.classList.contains("img-opacity")) {
         imgMini3.classList.remove("img-opacity");
         idFocus3.classList.remove("focus")
-        imgBox3.classList.remove("img-main-animation3")
+        idImgMain3.classList.remove("img-main-animation3")
     } else {
-        imgBox3.classList.add("img-main-animation3")
-        imgBox2.classList.remove("img-main-animation2")
-        imgBox1.classList.remove("img-main-animation1")
-        imgBox4.classList.remove("img-main-animation4")
-        imgBox3.classList.remove("img-box-invisible")
-        imgBox2.classList.add("img-box-invisible")
-        imgBox1.classList.add("img-box-invisible")
-        imgBox4.classList.add("img-box-invisible")
+        idImgMain3.classList.add("img-main-animation3")
+        idImgMain2.classList.remove("img-main-animation2")
+        idImgMain1.classList.remove("img-main-animation1")
+        idImgMain4.classList.remove("img-main-animation4")
+        idImgMain3.classList.remove("img-box-invisible")
+        idImgMain2.classList.add("img-box-invisible")
+        idImgMain1.classList.add("img-box-invisible")
+        idImgMain4.classList.add("img-box-invisible")
         imgMini3.classList.add("img-opacity");
         idFocus3.classList.add("focus")
         idFocus2.classList.remove("focus")
@@ -90,20 +94,21 @@ imgMini3.addEventListener("click", function () {
     }
 })
 imgMini4.addEventListener("click", function () {
+    imgPredeterminada.classList.add("img-box-invisible")
     /* idImgMain.setAttribute("src", "img/imgCarrito/image-product-4.jpg") */
     if (imgMini4.classList.contains("img-opacity")) {
         imgMini4.classList.remove("img-opacity");
         idFocus4.classList.remove("focus")
-        imgBox4.classList.remove("img-main-animation4")
+        idImgMain4.classList.remove("img-main-animation4")
     } else {
-        imgBox4.classList.add("img-main-animation4")
-        imgBox2.classList.remove("img-main-animation2")
-        imgBox3.classList.remove("img-main-animation3")
-        imgBox1.classList.remove("img-main-animation1")
-        imgBox4.classList.remove("img-box-invisible")
-        imgBox2.classList.add("img-box-invisible")
-        imgBox1.classList.add("img-box-invisible")
-        imgBox3.classList.add("img-box-invisible")
+        idImgMain4.classList.add("img-main-animation4")
+        idImgMain2.classList.remove("img-main-animation2")
+        idImgMain3.classList.remove("img-main-animation3")
+        idImgMain1.classList.remove("img-main-animation1")
+        idImgMain4.classList.remove("img-box-invisible")
+        idImgMain2.classList.add("img-box-invisible")
+        idImgMain1.classList.add("img-box-invisible")
+        idImgMain3.classList.add("img-box-invisible")
         imgMini4.classList.add("img-opacity");
         idFocus4.classList.add("focus")
         idFocus2.classList.remove("focus")
