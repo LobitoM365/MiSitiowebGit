fc11eed863c609464f8f0fedacc4a39dd242db42
LobitoM365 <almariojuancho@gmail.com>
2023-05-09 08:15:41 -0500

    probando modificaciones

--- js/sliderImg.js
+++ js/sliderImg.js
@@ -1,28 +1,92 @@
 let img = document.querySelectorAll(".img");
 let chevron = document.querySelectorAll(".chevron");
+let contador = document.getElementById("contador");
 
 
 
-let contador = -1;
-for (let u = 2; u < 5; u++) {
-    for (let i = 0; i < chevron.length; i++) {
-        chevron[i].addEventListener("click", function () {
-            if (i == 1) {
-                for (let x = 0; x < img.length; x++) {
-                    console.log(x - 1)
+for (let i = 0; i < chevron.length; i++) {
+
+    chevron[i].addEventListener("click", function () {
+
+
+        if (i == 1) {
+
+            if (contador.innerHTML < 2) {
+                contadorRight = parseFloat(contador.innerHTML) + 1;
+                document.getElementById("contador").innerHTML = contadorRight;
+                console.log(contadorRight)
+            } else
+
+                document.getElementById("contador").innerHTML = 0;
+        }
+        for (let x = 0; x < img.length; x++) {
+            if (contador.innerHTML == x) {
+
+                img[x].classList.remove("transition" + (x - 1));
+                img[x].classList.remove("transition" + (x - 2));
+                img[contador.innerHTML].classList.add("transition0");
+                img[x].classList.remove("transition" + x);
+
+
+            } else {
+
+                if (contador.innerHTML == 1) {
                     img[x].classList.remove("transition" + x);
-                    img[x].classList.remove("transition" + (x + 1));
                     img[x].classList.add("transition" + (x - 1));
-                }
+                } else
+                    if (contador.innerHTML == 2) {
+                        img[x].classList.remove("transition" + (x - 1));
+                        img[x].classList.add("transition" + (x - 2));
+                    }
+                    else
+                        if (contador.innerHTML == 0) {
+                            img[0].classList.add("transition0");
+                            img[x].classList.remove("transition" + (x - 2));
+                            img[x].classList.add("transition" + x);
+                        }
             }
-            if (i == 0) {
 
-                for (let x = 0; x < img.length; x++) {
-                    img[x].classList.remove("transition" + x);
-                    img[x].classList.add("transition" + (x + 1));
-                    
-                }
+
+        }
+        if (i == 0) {
+            if (contador.innerHTML > 0) {
+                contadorRight = parseFloat(contador.innerHTML) - 1;
+                document.getElementById("contador").innerHTML = contadorRight;
+            } else {
+
+                document.getElementById("contador").innerHTML = 2;
             }
-        })
-    }
+        } for (let x = 0; x < img.length; x++) {
+            if (contador.innerHTML == x) {
+
+                img[x].classList.remove("transition" + (x - 1));
+                img[x].classList.remove("transition" + (x - 2));
+                img[contador.innerHTML].classList.add("transition0");
+                img[x].classList.remove("transition" + x);
+
+
+            } else {
+
+                if (contador.innerHTML == 1) {
+                    img[x].classList.add("transition" + (x - 1));
+                    img[x].classList.remove("transition" + (x - 2));
+                } else
+                    if (contador.innerHTML == 2) {
+                        img[x].classList.add("transition" + (x - 2));
+                        img[x].classList.remove("transition" + x);
+
+                    }
+                    else
+                        if (contador.innerHTML == 0) {
+                            img[0].classList.add("transition0");
+                            img[x].classList.add("transition" + x);
+                            img[x].classList.remove("transition" + (x - 1));
+                        }
+            }
+
+
+        }
+
+    })
 }
+
