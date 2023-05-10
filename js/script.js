922b46d9a6104b225da4ecf7a50fd5b397966241
LobitoM365 <almariojuancho@gmail.com>
2023-05-10 09:48:01 -0500

    probando modificaciones

--- js/script.js
+++ js/script.js
@@ -2,23 +2,22 @@ let iconHamburguer = document.getElementById("icon_hamburguer");
 let menuPrincipal = document.getElementById("menuPrincipal");
 let iconSubMenuPrincipal = document.getElementById("iconSubMenuPrincipal");
 let subMenuPrincipal = document.getElementById("subMenuPrincipal");
-
 let toogle = document.getElementById("toogle");
+let iconSubMenu = document.querySelectorAll(".iconSubMenu");
+let subMenuInvisible = document.querySelectorAll(".subMenuInvisible");
 
-iconHamburguer.addEventListener("click", function(){
-    menuPrincipal.classList.toggle("invisible")
+iconHamburguer.addEventListener("click", function () {
+    menuPrincipal.classList.toggle("invisible-mobile")
 })
-iconSubMenuPrincipal.addEventListener("click", function(){
-    subMenuPrincipal.classList.toggle("invisible")
-})
+
 
 
 let circuloToogle = document.getElementById("circuloToogle");
 let ulWhite = document.getElementById("ulWhite");
-let imgFondoPcBlack= document.getElementById("imgFondoPcBlack");
-let imgFondoPcWhite= document.getElementById("imgFondoPcWhite");
+let imgFondoPcBlack = document.getElementById("imgFondoPcBlack");
+let imgFondoPcWhite = document.getElementById("imgFondoPcWhite");
 toogle.addEventListener("click", function () {
-    if(circuloToogle.classList.contains("circuloToogleTransition")){
+    if (circuloToogle.classList.contains("circuloToogleTransition")) {
         circuloToogle.classList.remove("circuloToogleTransition")
         toogle.classList.remove("toogleTransition")
         ulWhite.classList.remove("ulWhite")
@@ -27,7 +26,7 @@ toogle.addEventListener("click", function () {
         containVisibilidad(imgFondoPcWhite);
         imgFondoPcWhite.classList.remove("transitionWhite")
         imgFondoPcBlack.classList.remove("transitionBlack")
-    }else{
+    } else {
         circuloToogle.classList.add("circuloToogleTransition")
         toogle.classList.add("toogleTransition")
         menuPrincipal.classList.add("navWhite")
@@ -39,3 +38,21 @@ toogle.addEventListener("click", function () {
     }
 })
 
+iconHamburguer.addEventListener("click", function () {
+    iconSubMenuPrincipal.addEventListener("click", function () {
+        subMenuPrincipal.classList.toggle("invisible-mobile")
+    })
+    for (let i = 0; i < iconSubMenu.length; i++) {
+        iconSubMenu[i].addEventListener("click", function () {
+            for (let x = 0; x < iconSubMenu.length; x++) {
+                if (x == i) {
+                    subMenuInvisible[x].classList.toggle("subMenuInvisible");
+                    subMenuInvisible[x].classList.toggle("prueba");
+                } else {
+                    subMenuInvisible[x].classList.add("subMenuInvisible");
+                    subMenuInvisible[x].classList.remove("prueba");
+                }
+            }
+        })
+    }
+})
