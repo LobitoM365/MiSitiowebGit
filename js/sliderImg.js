2822aadaf6bc3c69a903ce54894a9b2c83441917
LobitoM365 <almariojuancho@gmail.com>
2023-05-10 15:28:09 -0500

    probando modificaciones

--- js/sliderImg.js
+++ js/sliderImg.js
@@ -1,6 +1,8 @@
 let img = document.querySelectorAll(".img");
 let chevron = document.querySelectorAll(".chevron");
+let punto = document.querySelectorAll(".punto");
 let contador = document.getElementById("contador");
+let posicionImagen = document.getElementById("posicionImagen");
 
 
 
@@ -10,26 +12,28 @@ for (let i = 0; i < chevron.length; i++) {
 
 
         if (i == 1) {
+            let numeroPosicionImagen = parseFloat(posicionImagen.innerHTML) + 1;
+            posicionImagen.innerHTML = numeroPosicionImagen;
+            if (posicionImagen.innerHTML > 3) {
+                posicionImagen.innerHTML = 1;
+            }
 
             if (contador.innerHTML < 2) {
                 contadorRight = parseFloat(contador.innerHTML) + 1;
                 document.getElementById("contador").innerHTML = contadorRight;
-                console.log(contadorRight)
-            } else
-
+            } else {
                 document.getElementById("contador").innerHTML = 0;
+            }
+
         }
         for (let x = 0; x < img.length; x++) {
             if (contador.innerHTML == x) {
-
+                punto[x].classList.add("puntoFocus");
                 img[x].classList.remove("transition" + (x - 1));
                 img[x].classList.remove("transition" + (x - 2));
-                
                 img[x].classList.remove("transition" + x);
-
-
             } else {
-
+                punto[x].classList.remove("puntoFocus");
                 if (contador.innerHTML == 1) {
                     img[x].classList.remove("transition" + x);
                     img[x].classList.add("transition" + (x - 1));
@@ -49,24 +53,27 @@ for (let i = 0; i < chevron.length; i++) {
 
         }
         if (i == 0) {
+            let numeroPosicionImagen = parseFloat(posicionImagen.innerHTML) - 1;
+            posicionImagen.innerHTML = numeroPosicionImagen;
+            if (posicionImagen.innerHTML < 1) {
+                posicionImagen.innerHTML = 3;
+            }
             if (contador.innerHTML > 0) {
                 contadorRight = parseFloat(contador.innerHTML) - 1;
                 document.getElementById("contador").innerHTML = contadorRight;
             } else {
                 document.getElementById("contador").innerHTML = 2;
             }
-          
+
         } for (let x = 0; x < img.length; x++) {
             if (contador.innerHTML == x) {
-
                 img[x].classList.remove("transition" + (x - 1));
                 img[x].classList.remove("transition" + (x - 2));
                 img[contador.innerHTML].classList.add("transition0");
                 img[x].classList.remove("transition" + x);
-
-
+                punto[x].classList.add("puntoFocus");
             } else {
-
+                punto[x].classList.remove("puntoFocus");
                 if (contador.innerHTML == 1) {
                     img[x].classList.add("transition" + (x - 1));
                     img[x].classList.remove("transition" + (x - 2));
@@ -74,7 +81,6 @@ for (let i = 0; i < chevron.length; i++) {
                     if (contador.innerHTML == 2) {
                         img[x].classList.add("transition" + (x - 2));
                         img[x].classList.remove("transition" + x);
-
                     }
                     else
                         if (contador.innerHTML == 0) {
@@ -83,10 +89,40 @@ for (let i = 0; i < chevron.length; i++) {
                             img[x].classList.remove("transition" + (x - 1));
                         }
             }
-
-
         }
-
     })
 }
 
+for (let x = 0; x < punto.length; x++) {
+    punto[x].addEventListener("click", function () {
+        for (let u = 0; u < punto.length; u++) {
+            document.getElementById("contador").innerHTML = x;
+            if (x == u) {
+                console.log(x)
+                img[x].classList.remove("transition" + (x - 1));
+                img[x].classList.remove("transition" + (x - 2));
+                img[x].classList.remove("transition" + x);
+                img[x].classList.add("transition0");
+                punto[x].classList.add("puntoFocus");
+            } else {
+                punto[u].classList.remove("puntoFocus");
+                if (x == 0) {
+                    posicionImagen.innerHTML = 1;
+                    img[u].classList.add("transition" + u);
+                }
+                if (x == 1) {
+                    posicionImagen.innerHTML = 2;
+                    img[u].classList.remove("transition" + u);
+                    img[u].classList.add("transition" + (u - 1));
+                }
+                if (x == 2) {
+                    posicionImagen.innerHTML = 3;
+                    img[u].classList.remove("transition" + u);
+                    img[u].classList.add("transition" + (u - 2));
+                    img[u].classList.remove("transition" + (u - 1));
+                }
+
+            }
+        }
+    })
+}
